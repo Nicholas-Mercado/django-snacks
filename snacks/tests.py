0fa11eecc14486b96947c7d2de0893f05fdcfcf6
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-23 14:55:02 -0700

    home,about 200 passing

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from urllib import response
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SnackTests(SimpleTestCase):
+    
+    def test_home_status(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_about_status(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
